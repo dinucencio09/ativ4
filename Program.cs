59da68fef976054a660b708aa2f643d4c8c60066
dinucencio09 <133662891+dinucencio09@users.noreply.github.com>
2024-05-29 11:35:32 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,113 @@
+using System;
+namespace QuartaAtividade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*Primeiro Exercício: Login do Usuário*/
+            
+            /*
+            Console.WriteLine("Insira seu Usuário:");
+            string login = Console.ReadLine();
+
+            Console.WriteLine("\nInsira sua senha:");
+            string senha = Console.ReadLine();
+
+            if (login == "admin" && senha == "1234")
+            {
+                Console.WriteLine("\nPode Acessar");
+            }
+            else
+            {
+                Console.WriteLine("Login ou Senha Incorreta");
+            }
+            */
+            
+            
+            
+            /*Segundo Exercício: Estoque*/
+            
+            /*
+            Console.WriteLine("Insira a Quantitade de Blusas no Estoque:");
+            int qntEstoque = int.Parse(Console.ReadLine());
+
+            if (qntEstoque > 40)
+            {
+                Console.WriteLine("Estoque OK");
+            }
+            else if (qntEstoque > 20 && qntEstoque < 40)
+            {
+                Console.WriteLine("Atenção ao Estoque");
+            }
+            else
+            {
+                Console.WriteLine("Estoque Crítico");
+            }
+            */
+            
+            
+            
+            /*Terceiro Exercício: Visto*/
+            
+            /*
+            Console.WriteLine("Insira sua Idade:");
+            int idadeVistoViagem = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira seu Salário:");
+            double salarioVistoViagem = double.Parse(Console.ReadLine());
+
+            if (idadeVistoViagem >= 18 && salarioVistoViagem > 2000)
+            {
+                Console.WriteLine("\nPode viajar");
+            }
+            else
+            {
+                Console.WriteLine("\nNão pode Viajar");
+            }
+            */
+
+
+
+            /*Quarto Exercício: Votação*/
+
+            /*
+            Console.WriteLine("Vote na Aula do Mês: \nOs votos 1 e 2 são para: Desenho e Pintura; \nOs votos 3 e 4 são para Discurso e Debate; \nOutros Valores de Voto são para Dança.");
+
+            Console.WriteLine("\nInsira seu Voto:");
+            int voto = int.Parse(Console.ReadLine());
+
+            if (voto == 1 || voto == 2)
+            {
+                Console.WriteLine("Desenho e Pintura");
+            }
+            else if (voto == 3 || voto == 4)
+            {
+                Console.WriteLine("Discurso e Debate");
+            }
+            else
+            {
+                Console.WriteLine("Dança");
+            }
+            */
+
+
+
+            /*Quinto Exercício: 100+*/
+
+            /*
+            Console.WriteLine("Insira um valor: ");
+            int valor = int.Parse(Console.ReadLine());
+
+            if (valor > 0 && valor > 100)
+            {
+                Console.WriteLine("Valor Especial");
+            }
+            else
+            {
+                Console.WriteLine("Fora do intervalo");
+            */
+        }
+    }
+}
